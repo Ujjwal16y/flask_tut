6dc056c59ccef86090f8d29301bd8a8aa35af235
ujjwaly <ujjwalyadav1610@gmail.com>
2022-05-07 11:57:45 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+model=pickle.load(open('model.pkl','rb'))
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    #values =[int(x) for x in request.form.values()]
+    ram=int(request.form.get("ram"))
+    rom=int(request.form.get("rom"))
+    ss=float(request.form.get("size"))
+    pc=int(request.form.get("back"))
+    bc=int(request.form.get("front"))
+    battery=int(request.form.get("battery"))
+    values=[ram,rom,ss,pc,bc,battery]
+    final=np.array(values)
+    prediction =model.predict(final.reshape(1,-1))
+    output=round(prediction[0],2)
+    return render_template('index.html',sent_value=f"Expected price of the phone is {output}")
+if __name__ == '__main__':
+    app.run(debug=True)
